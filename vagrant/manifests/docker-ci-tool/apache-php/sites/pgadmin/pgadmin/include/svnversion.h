4db45a567df00deb88e0c31caa236ded2c18292e
Gilberto Santos <gilbertopsantosjr@Gilbertos-MacBook-Pro.local>
2016-08-30 22:43:22 +0100

    vagrant leve 1000 Gb

--- vagrant/manifests/docker-ci-tool/apache-php/sites/pgadmin/pgadmin/include/svnversion.h
+++
@@ -1,1 +0,0 @@
-#define VERSION_SVN "REL-1_22_1"
--- vagrant/manifests/docker.pp
+++ vagrant/manifests/docker.pp
@@ -5,26 +5,131 @@ class base {
 
   ## Update apt-get ##
   exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command => '/usr/bin/apt-get update && apt-get upgrade -y'
+  }
+  
+}
+
+class http {
+
+  define apache::loadmodule () {
+    exec { "/usr/sbin/a2enmod $name" :
+      unless => "/bin/readlink -e /etc/apache2/mods-enabled/${name}.load",
+      notify => Service[apache2]
+    }
+  }
+
+  apache::loadmodule{"rewrite":}
+
+  package { "apache2":
+    ensure => present,
+  }
+
+  service { "apache2":
+    ensure => running,
+    require => Package["apache2"],
   }
 }
 
-class ci {
-    exec{ "create-ci-env":
-        command => "/vagrant/manifests/docker-ci-tool/ ./setup.sh",
-        unless => "sudo docker run hello-world",
-        path => "/usr/bin",
-        require => Service["docker"]
-    }
+class php{
 
-    service{ "docker":
-    	ensure => running,
-    	enable => true,
-    	hasstatus => true,
-    	hasrestart => true,
-    	ensure => present,
-    }
+  package { "php5":
+    ensure => present,
+  }
+
+  package { "php5-cli":
+    ensure => present,
+  }
+
+  package { "php5-xdebug":
+    ensure => present,
+  }
+
+  package { "php5-mysql":
+    ensure => present,
+  }
+
+  package { "php5-imagick":
+    ensure => present,
+  }
+
+  package { "php5-mcrypt":
+    ensure => present,
+  }
+
+  package { "php-pear":
+    ensure => present,
+  }
+
+  package { "php5-dev":
+    ensure => present,
+  }
+
+  package { "php5-curl":
+    ensure => present,
+  }
+
+  package { "php5-sqlite":
+    ensure => present,
+  }
+
+  package { "libapache2-mod-php5":
+    ensure => present,
+  }
+
+}
+
+class mysql{
+
+  package { "mysql-server":
+    ensure => present,
+  }
+
+  service { "mysql":
+    ensure  => running,
+    require => Package["mysql-server"],
+    notify  => Exec["set-mysql-password"],
+  }
+
+  exec { "set-mysql-password":
+    command => "mysqladmin -u root password root",
+  }
+}
+
+class phpmyadmin{
+
+  package
+  {
+    "phpmyadmin":
+      ensure => present,
+      require => [
+        Exec['apt-get update'],
+        Package["php5", "php5-mysql", "apache2"],
+      ]
+  }
+  
+  file
+  {
+    "/etc/apache2/conf-available/phpmyadmin.conf":
+      ensure => link,
+      target => "/etc/phpmyadmin/apache.conf",
+      require => Package['apache2'],
+      notify => Exec["load_phpmyadmin_conf"],
+  }
+
+  exec { "load_phpmyadmin_conf":
+    command => "/usr/sbin/a2enconf phpmyadmin",
+    notify  => Exec["reload_apache"]
+  }
+  
+  exec { "reload_apache":
+    command => "/etc/init.d/apache2 reload",
+  }
+  
 }
 
 include base
-include ci
+include http
+include php
+include mysql
+include phpmyadmin
